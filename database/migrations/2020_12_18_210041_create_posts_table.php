ed5fc55e1b31f6f4386be06d86e784c8f686ce7c
Arash Jafari <arashrj@gmail.com>
2020-12-19 01:31:59 +0330

    added Post models and migration

---
+++ database/migrations/2020_12_18_210041_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('email')->nullable();
+            $table->string('name', 25)->nullable();
+            $table->text('content');
+            $table->string('secret', 30)->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
