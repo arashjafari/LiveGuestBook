f5618551c95975c4885c1d35ccaccf57f5a44598
Arash Jafari <arashrj@gmail.com>
2020-12-19 04:31:08 +0330

    fixed layout for mobile screens

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,13 +4,13 @@
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <title>{{ $title ?? 'Live Guestbook' }}</title> 
+        <title>{{ $title ?? __('Live Guestbook') }}</title> 
         @livewireStyles
     </head>
     <body class="bg-gray-800">
 
         <header>
-            <div class="container mx-auto px-5 py-8 max-w-screen-md bg-black mt-5 rounded-t-lg text-gray-300">
+            <div class="container mx-auto px-5 py-8 max-w-screen-md bg-black md:mt-5 rounded-t-lg text-gray-300">
                 <h2 class="text-2xl">{{ __('Live Guestbook') }}</h2>
                 <p class="text-gray-400">{{ __('You can write anything and then everyone can read it !') }}</p>
             </div>
@@ -20,11 +20,11 @@
             {{ $slot }}
         </div>
          
-        <header>
-            <div class="container mx-auto px-5 py-5 max-w-screen-md bg-black mb-5 rounded-b-lg text-gray-500 text-center text-sm">
-                <h2>Live Guestbook</h2>
+        <footer>
+            <div class="container mx-auto px-5 py-5 max-w-screen-md bg-black md:mb-5 rounded-b-lg text-gray-500 text-center text-sm">
+                <small>&#169; {{ __('Copyright') }} {{ date("Y") }}</small>
             </div>
-        </header>
+        </footer>
 
         @livewireScripts
     </body>
